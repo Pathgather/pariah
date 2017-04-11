5e898234b68c4141d084274c31b5618f77b0d8bf
Chris Hanks <christopher.m.hanks@gmail.com>
2017-04-11 17:45:07 -0400

    Support specifying indices as strings or callables.

--- lib/pariah/dataset/query.rb
+++ lib/pariah/dataset/query.rb
@@ -80,7 +80,16 @@ module Pariah
 
       def indices_as_string
         if indices = @query[:indices]
-          indices.join(',')
+          resolved =
+            indices.map do |index|
+              if index.respond_to?(:call)
+                index.call
+              else
+                index
+              end
+            end
+
+          resolved.join(',')
         else
           :_all
         end
