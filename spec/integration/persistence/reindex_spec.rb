e357f353b68b5d3b1c4559ab210638f2a624ec03
Chris Hanks <christopher.m.hanks@gmail.com>
2017-05-16 11:35:20 -0400

    Finish DRYing up reindex specs.

--- spec/integration/persistence/reindex_spec.rb
+++ spec/integration/persistence/reindex_spec.rb
@@ -39,13 +39,60 @@ describe Pariah::Dataset, "#reindex" do
           )
         end
 
-        it "should recover properly from an error"
+        describe "when the block throws an error" do
+          def get_aliases
+            FTS.send(
+              :execute_request,
+              method: :get,
+              path: ['pariah_index_1', '_aliases'],
+              allowed_codes: [200, 404],
+            )
+          end
+
+          def get_records
+            TestIndex.all.sort_by{|r| r[:title]}
+          rescue Pariah::Error => e
+            if e.message =~ /index_not_found_exception/
+              :index_not_found
+            else
+              raise e
+            end
+          end
+
+          it "should recover properly" do
+            aliases_before = get_aliases
+            records_before = get_records
+
+            error = assert_raises(RuntimeError) do
+              TestIndex.reindex do |ds|
+                ds.type(:pariah_type_1).upsert([
+                  {title: "Title 4", comments_count: 5},
+                  {title: "Title 5", comments_count: 9},
+                ])
+
+                raise "Hell!"
+              end
+            end
+
+            assert_equal "Hell!", error.message
+
+            aliases_after = get_aliases
+            records_after = get_records
+
+            assert_equal aliases_before, aliases_after
+            assert_equal records_before, records_after
+          end
+        end
       end
     end
   end
 
   describe "when the index hasn't been created yet" do
     include ItReindexesProperly
+
+    before do
+      TestIndex.drop_index
+    end
   end
 
   describe "when there is an index with the given name already in place" do
@@ -69,154 +116,6 @@ describe Pariah::Dataset, "#reindex" do
           {title: "Title 1", comments_count: 5},
           {title: "Title 2", comments_count: 9},
         ])
-      end
-    end
-  end
-
-  it "should create a new timestamped index and alias it to the given name" do
-    new_index_name = nil
-
-    result =
-      TestIndex.reindex do |ds|
-        new_index_name = ds.send(:single_index)
-
-        ds.type(:pariah_type_1).upsert([
-          {title: "Title 1", comments_count: 5},
-          {title: "Title 2", comments_count: 9},
-        ])
-      end
-
-    assert_equal ["Title 1", "Title 2"], TestIndex.map{|r| r[:title]}.sort
-    assert_equal true, result
-
-    assert_match(/\Apariah_index_1-\d{10,12}\.\d+\z/, new_index_name)
-
-    aliases =
-      FTS.send(
-        :execute_request,
-        method: :get,
-        path: [new_index_name, '_aliases']
-      )
-
-    assert_equal(
-      {new_index_name.to_sym => {aliases: {pariah_index_1: {}}}},
-      aliases
-    )
-  end
-
-  it "should also work if the index being replaced is an alias" do
-    new_index_name = nil
-
-    TestIndex.reindex do |ds|
-      ds.type(:pariah_type_1).upsert([
-        {title: "Title 1", comments_count: 5},
-        {title: "Title 2", comments_count: 9},
-      ])
-    end
-
-    TestIndex.reindex do |ds|
-      new_index_name = ds.send(:single_index)
-
-      ds.type(:pariah_type_1).upsert([
-        {title: "Title 3", comments_count: 5},
-        {title: "Title 4", comments_count: 9},
-      ])
-    end
-
-    assert_equal ["Title 3", "Title 4"], TestIndex.map{|r| r[:title]}.sort
-
-    aliases =
-      FTS.send(
-        :execute_request,
-        method: :get,
-        path: [new_index_name, '_aliases']
-      )
-
-    assert_equal(
-      {new_index_name.to_sym => {aliases: {pariah_index_1: {}}}},
-      aliases
-    )
-  end
-
-  it "should also work if the index didn't previously exist" do
-    TestIndex.drop_index
-
-    TestIndex.reindex do |ds|
-      ds.type(:pariah_type_1).upsert([
-        {title: "Title 1", comments_count: 5},
-        {title: "Title 2", comments_count: 9},
-      ])
-    end
-
-    assert_equal ["Title 1", "Title 2"], TestIndex.map{|r| r[:title]}.sort
-  end
-
-  describe "when raising an error" do
-    it "should destroy the temporary index and leave the original index intact" do
-      store [
-        {title: "Title 1", comments_count: 5},
-        {title: "Title 2", comments_count: 9},
-        {title: "Title 3", comments_count: 5},
-      ]
-
-      error = assert_raises(RuntimeError) do
-        TestIndex.reindex do |ds|
-          ds.type(:pariah_type_1).upsert([
-            {title: "Title 4", comments_count: 5},
-            {title: "Title 5", comments_count: 9},
-          ])
-
-          raise "Hell!"
-        end
-      end
-
-      assert_equal "Hell!", error.message
-      assert_equal ["Title 1", "Title 2", "Title 3"], TestIndex.map{|r| r[:title]}.sort
-
-      aliases =
-        FTS.send(
-          :execute_request,
-          method: :get,
-          path: ['pariah_index_1', '_aliases']
-        )
-
-      assert_equal({pariah_index_1: {aliases: {}}}, aliases)
-    end
-
-    describe "when the target index is already an alias" do
-      it "should behave the same way" do
-        index_alias_name = nil
-        TestIndex.reindex do |ds|
-          index_alias_name = ds.send(:single_index)
-
-          ds.type(:pariah_type_1).upsert([
-            {title: "Title 1", comments_count: 5},
-            {title: "Title 2", comments_count: 9},
-          ])
-        end
-
-        error = assert_raises(RuntimeError) do
-          TestIndex.reindex do |ds|
-            ds.type(:pariah_type_1).upsert([
-              {title: "Title 4", comments_count: 5},
-              {title: "Title 5", comments_count: 9},
-            ])
-
-            raise "Hell!"
-          end
-        end
-
-        assert_equal "Hell!", error.message
-        assert_equal ["Title 1", "Title 2"], TestIndex.map{|r| r[:title]}.sort
-
-        aliases =
-          FTS.send(
-            :execute_request,
-            method: :get,
-            path: ['pariah_index_1', '_aliases']
-          )
-
-        assert_equal({index_alias_name.to_sym => {aliases: {pariah_index_1: {}}}}, aliases)
       end
     end
   end
